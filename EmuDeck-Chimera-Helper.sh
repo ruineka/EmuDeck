3c750eb826146e3430e9b97c69b046136eb6edde
ruineka <ruinairas@1992@gmail.comm>
2022-09-15 19:30:19 -0600

    Fixed an issue with stable and fuse.

--- EmuDeck-Chimera-Helper.sh
+++ EmuDeck-Chimera-Helper.sh
@@ -5,6 +5,8 @@ echo "Unlocking the system to install dependencies for EmuDeck, this likely won'
 echo "Enter the system password: Default is pass is gamer"
 sudo frzr-unlock
 sudo pacman --noconfirm -S zenity
+echo "for some reason fuse isn't working on stable, but is working on unstable..fixing this"
+sudo pacman --noconfirm -S fuse
 ./install.sh
  
 cp SteamRomManager.desktop /home/gamer/.local/share/applications
