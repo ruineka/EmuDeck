3f3cd15aa072be3decd046b37631b19d11bfbfb9
ruineka <ruinairas1992@gmail.com>
2022-09-18 12:14:53 -0500

    Update EmuDeck-Chimera-Helper.sh

--- EmuDeck-Chimera-Helper.sh
+++ EmuDeck-Chimera-Helper.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #EmuDeck install helper for ChimeraOS
 
-echo "Unlocking the system to install dependencies for EmuDeck, this likely won't be needed in the future"
-echo "Enter the system password: Default is pass is gamer"
-sudo frzr-unlock
-sudo pacman --noconfirm -S zenity
-echo "for some reason fuse isn't working on stable, but is working on unstable..fixing this"
-sudo pacman --noconfirm -S fuse
 ./install.sh
  
 cp SteamRomManager.desktop /home/gamer/.local/share/applications
